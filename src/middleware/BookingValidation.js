9e67e8bfb4ea8b08f9554d321b40053a8ea59018
samaha74 <mm.boodysamaha@gmail.com>
2026-05-04 17:46:32 +0300

    Finished booking Api(still needs testing)

---
+++ src/middleware/BookingValidation.js
@@ -0,0 +1,20 @@
+const booking = require('../models/Booking');
+
+
+exports.BookingIsValid = (req, res, next) => {
+    const { court, startTime, endTime, totalPrice } = req.body;
+
+    if (!court || !startTime || !endTime || !totalPrice) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+    
+    if (new Date(startTime) < new Date()) {
+        return res.status(400).json({ message: "You cannot book a court in the past!" });
+    }
+
+    if (new Date(startTime) >= new Date(endTime)) {
+        return res.status(400).json({ message: "End time must be after start time." });
+    }
+
+    next();
+}
